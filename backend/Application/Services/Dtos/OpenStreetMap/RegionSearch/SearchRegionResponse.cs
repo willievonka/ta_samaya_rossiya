5c301a724c55e293a91fd34fb169255fba338f64
daisnus <nikita.kozlov.200516@gmail.com>
2025-11-25 21:24:18 +0500

    Revert "Настройка для правильного pull request"
    This reverts commit 0aecaf3ca080f340726d31a24f00a2e8667548f2.

--- backend/Application/Services/Dtos/OpenStreetMap/RegionSearch/SearchRegionResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using Application.Services.Dtos.OpenStreetMap.SharedModels;
-
-namespace Application.Services.Dtos.OpenStreetMap.RegionSearch;
-
-public class SearchRegionResponse : BaseOverpassApiResponse
-{
-    public SearchRegionItem[] Elements { get; set; } = null!;
-}
