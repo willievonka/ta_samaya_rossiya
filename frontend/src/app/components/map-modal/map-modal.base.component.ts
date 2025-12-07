d61a305db73d90d358d984ca780ecf50e1bc92ae
Nikolai Klimov <nik.klimov09@yandex.ru>
2025-12-07 19:23:36 +0500

    Добавил фоллбэк для картинки в модалке карты

--- frontend/src/app/components/map-modal/map-modal.base.component.ts
+++ frontend/src/app/components/map-modal/map-modal.base.component.ts
@@ -7,4 +7,14 @@ import { IMapLayerProperties } from '../map/interfaces/map-layer.interface';
 export class MapModalBaseComponent {
     public readonly data: InputSignal<IMapLayerProperties> = input.required();
     public readonly closeModal: OutputEmitterRef<void> = output();
+
+    /**
+     * Фоллбэк для картинок
+     * @param event
+     * @param fallbackImagePath
+     */
+    protected handleImageFallback(event: Event, fallbackImagePath: string): void {
+        const img: HTMLImageElement = event.target as HTMLImageElement;
+        img.src = fallbackImagePath;
+    }
 }
