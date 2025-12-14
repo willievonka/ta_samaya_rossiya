f91697773acdb22bfec772685a239faf717061e2
Nikolai Klimov <nik.klimov09@yandex.ru>
2025-12-14 22:50:19 +0500

    Рефакторинг

--- frontend/src/app/components/map-page/map.page.base.component.ts
+++ frontend/src/app/components/map-page/map.page.base.component.ts
@@ -1,41 +1,29 @@
-import { computed, Directive, inject, Signal, signal, viewChild, WritableSignal } from '@angular/core';
+import { computed, Directive, inject, Signal, viewChild } from '@angular/core';
 import { IMapLayer } from '../map/interfaces/map-layer.interface';
 import { IMapZoomActions } from '../map/interfaces/map-zoom-actions.interface';
 import { MapComponent } from '../map/map.component';
 import { ActivatedRoute } from '@angular/router';
 import { MapDataService } from '../map/services/map-data.service';
-import { take, tap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { IMapModel } from '../map/models/map.model';
 
 @Directive({
     standalone: true
 })
 export class MapPageBaseComponent {
     protected readonly mapInstance: Signal<MapComponent | undefined> = viewChild(MapComponent);
-
-    protected readonly pageTitle: WritableSignal<string | undefined> = signal(undefined);
-    protected readonly infoText: WritableSignal<string | undefined> = signal(undefined);
-    protected readonly mapLayers: WritableSignal<IMapLayer[] | undefined> = signal(undefined);
     protected readonly zoomActions: Signal<IMapZoomActions | undefined> = computed(() => this.mapInstance()?.zoomActions());
 
+    protected readonly pageTitle: Signal<string | undefined> = computed(() => this._mapData()?.pageTitle);
+    protected readonly infoText: Signal<string | undefined> = computed(() => this._mapData()?.infoText);
+    protected readonly mapLayers: Signal<IMapLayer[] | undefined> = computed(() => this._mapData()?.layers);
+    protected readonly activeLayerColor: Signal<string | undefined> = computed(() => this._mapData()?.activeLayerColor);
+    protected readonly pointColor: Signal<string | undefined> = computed(() => this._mapData()?.pointColor);
+
     private readonly _mapDataService: MapDataService = inject(MapDataService);
-    private readonly _route: ActivatedRoute = inject(ActivatedRoute);
-
-    constructor() {
-        this.init();
-    }
-
-    /** Инициализация */
-    private init(): void {
-        const mapId: string = this._route.snapshot.queryParamMap.get('id') ?? '';
-        this._mapDataService.getMapData(mapId)
-            .pipe(
-                tap(data => {
-                    this.pageTitle.set(data.pageTitle);
-                    this.mapLayers.set(data.layers);
-                    this.infoText.set(data.infoText);
-                }),
-                take(1)
-            )
-            .subscribe();
-    }
+    private readonly _mapId: string = inject(ActivatedRoute).snapshot.queryParamMap.get('id') ?? '';
+    private readonly _mapData: Signal<IMapModel | undefined> = toSignal<IMapModel>(
+        this._mapDataService.getMapData(this._mapId),
+        { initialValue: undefined }
+    );
 }
