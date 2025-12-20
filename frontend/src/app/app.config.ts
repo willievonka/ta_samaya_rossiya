ecc4d60ee178cc401669857591c7be732facdfb3
Nikolai Klimov <nik.klimov09@yandex.ru>
2025-12-20 18:26:01 +0500

    Подключил taiga-ui

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,3 +1,5 @@
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -6,9 +8,11 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
     providers: [
+        provideAnimations(),
         provideBrowserGlobalErrorListeners(),
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideHttpClient()
+        provideHttpClient(),
+        provideEventPlugins()
     ]
 };
